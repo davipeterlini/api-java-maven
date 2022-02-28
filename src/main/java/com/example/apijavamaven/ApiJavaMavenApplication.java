4fa500de6c273a1249f775e7f46b0e467cad6224
Davi Peterlini <davi.peterlini@dextra-sw.com>
2022-02-28 12:26:41 -0300

    Adding New Code for Spring Initializr

---
+++ src/main/java/com/example/apijavamaven/ApiJavaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.apijavamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaMavenApplication.class, args);
+	}
+
+}
